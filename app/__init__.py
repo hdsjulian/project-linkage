7e5f2f13b4756e8b957b344fdad71c8afacc725f
Julian Finn <julian@phinn.de>
2020-02-07 13:49:35 -0500

    crsf

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+from flask_wtf.csrf import CsrfProtect
 
+csrf = CsrfProtect()
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +13,7 @@ app.debug = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
+csrf.init_app(app)
 
 #das muss hier unten stehen
 from app import routes, models
