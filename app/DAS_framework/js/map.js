fb408df9fbd2db283d65a3b92575c59964ae39fb
Markus Angermeier <kosmar@gmail.com>
2021-07-31 13:16:32 +0200

    rename #map, 100vh

--- app/DAS_framework/js/map.js
+++ app/DAS_framework/js/map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -7,4 +7,4 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoicHJvamVjdGxpbmthZ2UiLCJhIjoiY2tycGk0ejA2MmQ1cTJucnZiZTRsOXZlYyJ9.YkeSvM4CDDlz9dHlkc7Zuw'
 }).addTo(mymap);
-document.querySelector('#mapid').style.height = window.innerHeight+'px';
+//document.querySelector('#mapid').style.height = window.innerHeight+'px';
