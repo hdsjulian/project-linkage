ebc09c51e1a9db18cc77d8ed71f8ec0a577d3a63
JULIAN FINN <julian@JULIANs-MacBook-Air.local>
2021-08-01 22:58:57 +0200

    fixed import

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 import crud, models, schemas
-from .database import SessionLocal, engine
+from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
