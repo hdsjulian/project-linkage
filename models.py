8dfac8bb2a9e0a33c00c759932972414798eb237
JULIAN FINN <julian@julians-air.localdomain>
2021-08-02 13:45:53 +0200

    testing something

--- models.py
+++ models.py
@@ -13,19 +13,4 @@ class Coin(Base):
     __tablename__ = "coins"
     id = Column(Integer, primary_key=True, index=True)
     hash = Column(String, unique=True)
-    handovers = relationship("Handover", back_populates="coin")
-
-class Handover(Base):
-    __tablename__ = "handovers"
-    id = Column(Integer, primary_key=True, index=True)
-    lat = Column(Float)
-    lon = Column(Float)
-    text = Column(Text)
-    timestamp = Column(Integer, index=True, default = datetime.utcnow)
-    predecessor_id = Column(Integer, ForeignKey("handovers.id"))
-    recipient_id = Column (Integer, ForeignKey("users.id"))
-    giver_id = Column (Integer, ForeignKey("users.id"))
-    giver = relationship("User", back_populates="handovers_given")
-    recipient = relationship("User", back_populates="handovers_received")
-    
 
