13d82c85c456cc38399cfa7ff8405c45e9a061f7
JULIAN FINN <julian@julians-air.localdomain>
2021-08-02 14:19:24 +0200

    get coins

--- main.py
+++ main.py
@@ -35,4 +35,7 @@ def read_coin(coin_id: int, db: Session=Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return db_coin
 
-@app.get("coins", response_mode-)
+@app.get("/coins/", response_model=List[schemas.Coin])
+def read_coins(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    coins = crud.get_coins(db, skip=skip, limit=limit)
+    return coins
