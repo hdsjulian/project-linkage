1beedb095cd180f5e2ebf6e5c56589541fce2039
JULIAN FINN <julian@julians-air.localdomain>
2021-08-05 22:57:03 +0200

    sadg

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import os, sys
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 print("------------------------------")
 print("------------------------------")
 print("------------------------------")
