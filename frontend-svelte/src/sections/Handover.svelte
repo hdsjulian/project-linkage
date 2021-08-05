a5f8c3d8fbaa358304ba4c83df06c7d6b18f54c4
Daniel Reszka <dannn.r@gmail.com>
2021-08-05 18:27:01 +0200

    fix missing async handling

--- frontend-svelte/src/sections/Handover.svelte
+++ frontend-svelte/src/sections/Handover.svelte
@@ -1,16 +1,12 @@
 <script>
-    import api from "../api"
-  
-    let getHandover = api.list("/handover/1")
-  
-    // declare form values
-    
-  
-    // set default values
-    title = `Random  ${Math.floor(Math.random() * 10000)}`
-    author = "Tom"
-  </script>
-  
+  import api from "../api"
+
+  let getHandover = api.list("/handovers/1")
+</script>
+
+{#await getHandover}
+  <p>Loading...</p>
+{:then handover}
   <dl>
     <dt>Handover number</dt>
     <dd>{handover.id}</dd>
@@ -25,17 +21,21 @@
     <dt>at Lon:Lat</dt>
     <dd>55.458:25.873</dd>
     <dt>What they had to say to each other</dt>
-    <dd>This is an experiment about trust. We have been friends from age six.
-        We trust each other and love each other and will be there for each other 
-        however long our life will go, in whatever situation we are. 
-        We were bridesmaids at our weddings and we will be buried together in the same grave because besties </dd>
-
+    <dd>
+      This is an experiment about trust. We have been friends from age six. We
+      trust each other and love each other and will be there for each other
+      however long our life will go, in whatever situation we are. We were
+      bridesmaids at our weddings and we will be buried together in the same
+      grave because besties
+    </dd>
   </dl>
-  
-  <nav class="paging">
-    <ul class="paging__list">
-      <li class="previous"><a>Previous</a></li>
-      <li class="next"><a>Afterwards</a></li>
-    </ul>
-  </nav>
-  
+{:catch error}
+  <p class="error"><b>Error</b> <small>{error.message}</small></p>
+{/await}
+
+<nav class="paging">
+  <ul class="paging__list">
+    <li class="previous"><a>Previous</a></li>
+    <li class="next"><a>Afterwards</a></li>
+  </ul>
+</nav>
