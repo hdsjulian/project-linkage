f8449ee02f3ce37f6f0f63a5d96dc06f411d6e74
Markus Angermeier <kosmar@gmail.com>
2021-08-08 21:11:35 +0200

    ul li

--- frontend-svelte/src/pages/index.svelte
+++ frontend-svelte/src/pages/index.svelte
@@ -1,14 +1,15 @@
 <section class="about" id="about">
   <p>Project linkage is an experiment about human trust.</p>
   <p>
-    A number of initial participants received coins with an individual QR code
-    on the back. They have five basic tasks:
+    A number of initial participants received coins with an individual QR code on the back. They have five basic tasks:
   </p>
-  <ul>Hand this coin to a person they trust</ul>
-  <ul>Have a conversation about their mutual relationship</ul>
-  <ul>Scan the QR code and document the handover</ul>
-  <ul>Write a short text about their conversation for everyone to read</ul>
-  <ul>Request that the receiving person repeats these five steps</ul>
+  <ul>
+    <li>Hand this coin to a person they trust
+    <li>Have a conversation about their mutual relationship
+    <li>Scan the QR code and document the handover
+    <li>Write a short text about their conversation for everyone to read
+    <li>Request that the receiving person repeats these five steps
+  </ul>
   <p>Thus, we create chains based on human trust. And tell our stories.</p>
   <a href="#coin">Coin</a>
 </section>
