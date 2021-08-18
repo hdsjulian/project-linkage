ed6606ce43d966f10f3d801bded2cbd9057c57c5
JULIAN FINN <julian@julians-air.localdomain>
2021-08-18 15:27:52 +0200

    privacy

--- frontend-svelte/src/pages/privacy.svelte
+++ frontend-svelte/src/pages/privacy.svelte
@@ -1,91 +1,94 @@
-<h1>Privacy / Datenschutz</h1>
+<span class="legal"><h1>Privacy</h1>
 
 <small>
-  <p>Deutsche Version <a href="#de">hier</a></p>
+  <p>Deutsche Version unter der englischen </p>
+  <p><br/></p>
 </small>
 <small>
-  <h3 id="#en">Names and contact data of the person responsible:
-  </h3>
+  <h2>Names and contact data of the person responsible:
+  </h2>
   <p>
     Julian Finn, julian[at]phinn[punkt]de
   </p>
-  <h3>Purposes for which the personal data are to be processed and the legal basis for the processing:
-  </h3>
+  <h2>Purposes for which the personal data are to be processed and the legal basis for the processing:
+  </h2>
   <p>
     All data is processed exclusively voluntarily and on the basis of consent (Art. 6 para. 1 lit. a DSGVO). In detail, these are: Name, email address, password, story entered by the user and the place of delivery. By giving your consent, you also agree that - if you hand over the received coin - your data record will be linked to the recipient's data record in such a way that the handover between you and the recipient can be traced and published as part of the project.
   </p>
   <p>
     The granting of consent is of course voluntary, if you do not consent, no data will be processed and participation in the art project will then not take place.
     </p>
-  <h3>Recipients or categories of recipients of the personal data: 
-  </h3>
+  <h2>Recipients or categories of recipients of the personal data: 
+  </h2>
   <p>
     Username and password will not be given to third parties, but will only be used to log in to the account again. Name, story and location will be published freely on the Internet at www.project-linkage.net as part of the art project.
     The e-mail address will only be used for notifications in the context of the project. 
   </p>
-  <h3>Duration for which the personal data will be stored or, if this is not possible, the criteria for determining this duration:
-  </h3>
+  <h2>Duration for which the personal data will be stored or, if this is not possible, the criteria for determining this duration:
+  </h2>
   <p>
     The data will be stored for the duration of the project, unless the consent given for data processing is revoked beforehand.
   </p>
-  <h3>The existence of a right of access to the personal data concerned, as well as the right to rectification or erasure or to restriction of processing:</h3>
+  <h2>The existence of a right of access to the personal data concerned, as well as the right to rectification or erasure or to restriction of processing:</h2>
   <p>You can request information about the personal data stored about you at any time. To do so, send a message from the email address used for registration to julian[at]phinn[dot]de  </p>
   <p>You can also use the same e-mail address to revoke the declaration of consent you have given and to request the deletion or anonymization of your data.
   </p>
-  <h3>The existence of a right to withdraw consent at any time without affecting the lawfulness of the processing carried out on the basis of consent until withdrawal:
-  </h3>
-  <p>The processing of your data is based on consent. You can revoke this at any time for the future. To do this, you can either send a message to julian[at]phinn[dot]de from the e-mail stored in your account or click on "Delete" in your account as soon as this function is available.</p>.
-  <h3>The existence of a right of appeal to a supervisory authority:
-  </h3>
+  <h2>The existence of a right to withdraw consent at any time without affecting the lawfulness of the processing carried out on the basis of consent until withdrawal:
+  </h2>
+  <p>The processing of your data is based on consent. You can revoke this at any time for the future. To do this, you can either send a message to julian[at]phinn[dot]de from the e-mail stored in your account or click on "Delete" in your account as soon as this function is available.</p>
+  <h2>The existence of a right of appeal to a supervisory authority:
+  </h2>
   <p>You have the right to complain to the competent supervisory authority at any time.
   </p>
-  <h3>The existence of automated decision-making, including profiling:
-  </h3>
+  <h2>The existence of automated decision-making, including profiling:
+  </h2>
   <p>An automated individual decision-making or profiling does not take place.
   </p>
+  <p> <br/> </p>
 
 </small>
-
+<h1>Datenschutz</h1>
 
 <small>
-  <h3 id="#de">Namen und die Kontaktdaten des Verantwortlichen sowie gegebenenfalls seines Vertreters:
-  </h3>
+  <h2 id="#de">Namen und die Kontaktdaten des Verantwortlichen sowie gegebenenfalls seines Vertreters:
+  </h2>
   <p>
     Julian Finn, julian[at]phinn[punkt]de
   </p>
-  <h3>Zwecke, für die die personenbezogenen Daten verarbeitet werden sollen, sowie die Rechtsgrundlage für die Verarbeitung:
-  </h3>
+  <h2>Zwecke, für die die personenbezogenen Daten verarbeitet werden sollen, sowie die Rechtsgrundlage für die Verarbeitung:
+  </h2>
   <p>
     Sämtliche Daten werden ausschließlich freiwillig und auf Basis einer Einwilligung (Art. 6 Abs. 1 lit. a DSGVO) verarbeitet. Im Einzelnen handelt es sich um: Name, E-Mail-Adresse, Passwort, vom Nutzer eingegebene Story sowie den Ort der Übergabe. Mit der Erteilung Ihrer Einwilligung stimmen Sie zudem zu, dass - sofern Sie den erhaltenen Coin übergeben - Ihr Datensatz mit dem Datensatz des Empfängers dergestalt verknüpft wird, dass die Übergabe zwischen Ihnen und dem Empfänger nachvollzogen werden kann und als Teil des Projektes veröffentlich wird.
   </p>
   <p>
     Die Erteilung der Einwilligung ist selbstverständlich freiwillig, willigen Sie nicht ein, werden keine Daten verarbeitet und eine Teilnahme am Kunstprojekt findet dann nicht statt.
   </p>
-  <h3>Empfänger oder Kategorien von Empfängern der personenbezogenen Daten: 
-  </h3>
+  <h2>Empfänger oder Kategorien von Empfängern der personenbezogenen Daten: 
+  </h2>
   <p>
     Nutzername und Passwort werden nicht an Dritte weitergegeben, sondern ausschließlich für den erneuten Login in den Account verwendet. Name, Story und Location werden als Teil des Kunstprojektes frei im Internet unter www.project-linkage.net veröffentlicht.
     Die E-Mail-Adresse wird nur für Benachrichtigungen im Kontext des Projektes verwendet. 
   </p>
-  <h3>Dauer, für die die personenbezogenen Daten gespeichert werden oder, falls dies nicht möglich ist, die Kriterien für die Festlegung dieser Dauer:
-  </h3>
+  <h2>Dauer, für die die personenbezogenen Daten gespeichert werden oder, falls dies nicht möglich ist, die Kriterien für die Festlegung dieser Dauer:
+  </h2>
   <p>
     Die Daten werden für die Laufzeit des Projektes gespeichert, sofern die zur Datenverarbeitung erteilte Einwilligung nicht zuvor widerrufen wird.
   </p>
-  <h3>Das Bestehen eines Rechts auf Auskunft über die betreffenden personenbezogenen Daten sowie auf Berichtigung oder Löschung oder auf Einschränkung der Verarbeitung:</h3>
+  <h2>Das Bestehen eines Rechts auf Auskunft über die betreffenden personenbezogenen Daten sowie auf Berichtigung oder Löschung oder auf Einschränkung der Verarbeitung:</h2>
   <p>Sie können jederzeit Auskunft über die zu Ihnen gespeicherten personenbezogenen Daten verlangen. Senden Sie hierzu von der für die Registrierung verwendeten Email Adresse eine Nachricht an julian[at]phinn[punkt]de  </p>
   <p>Unter der gleichen E-Mail-Adresse können Sie auch die erteilte Einwilligungserklärung widerrufen und die Löschung bzw. Anonymisierung Ihrer Daten verlangen.
   </p>
-  <h3>Das Bestehen eines Rechts, die Einwilligung jederzeit zu widerrufen, ohne dass die Rechtmäßigkeit der aufgrund der Einwilligung bis zum Widerruf erfolgten Verarbeitung berührt wird:
-  </h3>
+  <h2>Das Bestehen eines Rechts, die Einwilligung jederzeit zu widerrufen, ohne dass die Rechtmäßigkeit der aufgrund der Einwilligung bis zum Widerruf erfolgten Verarbeitung berührt wird:
+  </h2>
   <p>Die Verarbeitung Ihrer Daten beruht auf einer Einwilligung. Diese können Sie jederzeit für die Zukunft widerrufen. Hierzu können Sie entweder von der im Account hinterlegten E-Mail eine Nachricht an julian[at]phinn[punkt]de schicken oder aber in Ihrem Account auf “Löschen” klicken, sobald diese Funktion verfügbar ist.</p>
-  <h3>Das Bestehen eines Beschwerderechts bei einer Aufsichtsbehörde:
-  </h3>
+  <h2>Das Bestehen eines Beschwerderechts bei einer Aufsichtsbehörde:
+  </h2>
   <p>Ihnen steht jederzeit das Recht zu, sich bei der zuständigen Aufsichtsbehörde zu beschweren.
   </p>
-  <h3>Das Bestehen einer automatisierten Entscheidungsfindung einschließlich Profiling:
-  </h3>
+  <h2>Das Bestehen einer automatisierten Entscheidungsfindung einschließlich Profiling:
+  </h2>
   <p>Eine automatisierte Einzelfallentscheidung oder ein Profiling findet nicht statt.
   </p>
 </small>
 
+</span>
