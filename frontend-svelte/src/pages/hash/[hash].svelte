3e1b96c5f36b41f556be878c3280915c9761e2df
JULIAN FINN <julian@julians-air.localdomain>
2021-08-22 15:56:47 +0200

    location manual set

--- frontend-svelte/src/pages/hash/[hash].svelte
+++ frontend-svelte/src/pages/hash/[hash].svelte
@@ -12,52 +12,66 @@
   let recipientEmail = ""
   let recipientName = ""
   let dataProtection
-  let data_protection_checked = true;
+  let data_protection_checked = true
   let step = 0
   let handoverText = ""
-  let result 
-  let lat = 52.520815
-  let lon = 13.4094191
-  let wrong_pass = false
-  let password_match = true
+  let result
+  let fixedLat = 52.520815,
+    fixedLon = 13.4094191,
+    lat = 0,
+    lon = 0
   let coin
-  let error_submitting = false
-  let error_position = false
+  let error_submitting = false,
+    error_position = false,
+    error_location = false
   let email_correct = true
   let name_correct = true
-  let prevLat = 0
-  let prevLon = 0
-  function setLocation(position) {
-    lat = position.coords.latitude
-    lon = position.coords.longitude
+  let prevLat = 0,
+    prevLon = 0
+  let isCheckedLocation,
+    willChooseLocation = false
+  function setLocation() {
+    if (isCheckedLocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        lat = position.coords.latitude
+        lon = position.coords.longitude
+      }, locationError)
+    }
   }
+
   function locationError(error) {
     if (error.code == error.PERMISSION_DENIED) {
       error_position = true
     }
   }
 
-  const nextStep = async() => {
+  const nextStep = async () => {
     if (step == 1) {
       data_protection_checked = checkDataProtection()
       email_correct = checkEmail()
-      name_correct = checkName() 
+      name_correct = checkName()
 
-      if (data_protection_checked == true && email_correct == true && name_correct == true) {
-        navigator.geolocation.getCurrentPosition(setLocation, locationError)
-        step +=1
+      if (
+        data_protection_checked == true &&
+        email_correct == true &&
+        name_correct == true
+      ) {
+        step += 1
       }
-    }
-    else if (step == 2) {
+    } else if (step == 2) {
+      if (lat !== 0 && lon !== 0) {
+        step += 1
+      } else {
+        error_location = true
+      }
+    } else if (step == 3) {
       const isHandoverSubmitted = await submitHandover()
       if (isHandoverSubmitted) {
         history.replaceState({}, null, `/handover/${handover_id}`)
-      }
-      else {
+      } else {
         error_submitting = true
       }
-    }
-    else {
+    } else {
       step += 1
     }
   }
@@ -66,7 +80,7 @@
     step -= 1
   }
 
- /* const checkPass = async () => {
+  /* const checkPass = async () => {
     console.log("Checking Pass")
     result = await (api.post("/verify_user/", { "hash": hash, "password": giverPassword }))
     console.log(result.is_verified)
@@ -80,63 +94,73 @@
     }
   }*/
 
-const checkPasswordMatch = () => {
-  if (recipientPassword == recipientPasswordAgain){
-    return true
-  }
-  else {
+  const checkPasswordMatch = () => {
+    if (recipientPassword == recipientPasswordAgain) {
+      return true
+    } else {
       return false
-  }
-}
-
-const checkDataProtection = () => {
-  return dataProtection
-}
-
-const checkEmail = () => {
-  if (recipientEmail == "") {
-    return false
-  }
-  else {
-    return true
-  }
-}
-
-const checkName = () => {
-  if (recipientName == "") {
-    return false
-  }
-  else {
-    return true
-  }
-}
-
-  const submitHandover = async() => {
-    result = await(api.post("/submit_handover/", {
-      "hash": hash, 
-      "giver_password": giverPassword, 
-      "recipient_password": recipientPassword,
-      "recipient_name": recipientName, 
-      "text": handoverText, 
-      "recipient_email": recipientEmail, 
-      "lat": lat, 
-      "lon": lon
-    }))
-    if (result.is_saved == true) {
-        handover_id = result.handover_id
-        return true
-      }
-      else {
-        return false
-      }
     }
+  }
+  const chooseLocation = () => {
+    willChooseLocation = true
+    console.log("click")
+    myMap.on("click", addMarker)
+  }
 
+  function addMarker(e) {
+    // Add marker to map at click location; add popup window
+    console.log("marker")
+    var newMarker = L.marker(e.latlng, { icon: mapMarker }).addTo(myMap)
+    lat = e.latlng.Lat
+    lon = e.latlng.lng
+  }
+
+  const checkDataProtection = () => {
+    return dataProtection
+  }
+
+  const checkEmail = () => {
+    if (recipientEmail == "") {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  const checkName = () => {
+    if (recipientName == "") {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  const submitHandover = async () => {
+    result = await api.post("/submit_handover/", {
+      hash: hash,
+      giver_password: giverPassword,
+      recipient_password: recipientPassword,
+      recipient_name: recipientName,
+      text: handoverText,
+      recipient_email: recipientEmail,
+      lat: lat,
+      lon: lon,
+    })
+    if (result.is_saved == true) {
+      handover_id = result.handover_id
+      return true
+    } else {
+      return false
+    }
+  }
 
   $afterPageLoad(() => {
-    myMap.setView([lat,lon], 6)
+    myMap.setView([lat, lon], 6)
     hash = $params.hash
+    console.log(hash)
     api.get(`/hash/${hash}`).then((res) => {
-      travels = res.data.coin.travels == null? 0: res.data.coin.travels
+      travels = res.data.coin.travels == null ? 0 : res.data.coin.travels
+      console.log()
       coin = res.data.coin
       console.log(coin)
       api.get(`/coins/${coin.id}/handovers/`).then((hl_res) => {
@@ -146,34 +170,39 @@ const checkName = () => {
         let polyFill = hl_res?.map((val) => [val.lat, val.lon])
         let iterator = 0
         myMap.eachLayer((layer) => {
-            if (iterator > 1) { 
-                layer.remove();
-            }
-            iterator +=1
-        });
-        for (const line of hl_res) { 
-          L.marker([line.lat, line.lon], {icon: mapMarker}).addTo(myMap)
+          if (iterator > 1) {
+            layer.remove()
+          }
+          iterator += 1
+        })
+        for (const line of hl_res) {
+          L.marker([line.lat, line.lon], { icon: mapMarker }).addTo(myMap)
           if (prevLat != 0) {
-              var polyLine = L.polyline([[prevLat, prevLon], [line.lat, line.lon]], {color:'red', weight: 5}).addTo(window.myMap)
+            var polyLine = L.polyline(
+              [
+                [prevLat, prevLon],
+                [line.lat, line.lon],
+              ],
+              { color: "red", weight: 5 }
+            ).addTo(window.myMap)
           }
           prevLat = line.lat
           prevLon = line.lon
         }
-
-      })      
+      })
     })
     console.log(coin)
 
-      //if there is a handover, get handover and display form. if not, just display form and display instructions
-      
-      //travels = coin.travels
+    //if there is a handover, get handover and display form. if not, just display form and display instructions
 
-      //   recipientName = handover.recipient.name
-      // giverName = handover.giver.name
-      //lat = handover.lat
-      //lon = handover.lon
-      //console.log(coin)
-    myMap.setView([lat, lon], 10)
+    //travels = coin.travels
+
+    //   recipientName = handover.recipient.name
+    // giverName = handover.giver.name
+    //lat = handover.lat
+    //lon = handover.lon
+    //console.log(coin)
+    myMap.setView([fixedLat, fixedLon], 10)
   })
 </script>
 
@@ -183,7 +212,8 @@ const checkName = () => {
       <p>
         <strong>Congratulations!</strong>
 
-        You have received this coin as part of Project linkage - an art project about trust! 
+        You have received this coin as part of Project linkage - an art project
+        about trust!
 
         <strong>
           Please read the instructions <strong>carefully</strong> and then click
@@ -191,47 +221,59 @@ const checkName = () => {
         </strong>
       </p>
       <p>
-       <strong>IMPORTANT!</strong> this is an art project. We will collect data but we will never abuse it for commercial purposes. 
-        We will, however, kindly ask you for your email address and your geolocation as well as a name. This is important for the project to work!
-        See more in the <a href="/privacy">privacy policy</a>
+        <strong>IMPORTANT!</strong> this is an art project. We will collect data
+        but we will never abuse it for commercial purposes. We will, however,
+        kindly ask you for your email address and your geolocation as well as a
+        name. This is important for the project to work! See more in the
+        <a href="/privacy">privacy policy</a>
       </p>
 
       <Rules />
     {:else}
       <strong>Welcome!</strong>
 
-      This coin has changed hands {travels} times so far 
+      This coin has changed hands {travels} times so far
 
       <p>
-        We assume you received this coin from your friend? If not, please contact us! Please make sure you
-        read the instructions again before you continue! 
+        We assume you received this coin from your friend? If not, please
+        contact us! Please make sure you read the instructions again before you
+        continue!
       </p>
       <p>
-        <strong>IMPORTANT!</strong> this is an art project. We will collect data but we will never abuse it for commercial purposes. 
-        We will, however, kindly ask you for your email address and your geolocation as well as a name. This is important for the project to work! See more in the <a href="/privacy">privacy policy</a>
+        <strong>IMPORTANT!</strong> this is an art project. We will collect data
+        but we will never abuse it for commercial purposes. We will, however,
+        kindly ask you for your email address and your geolocation as well as a
+        name. This is important for the project to work! See more in the
+        <a href="/privacy">privacy policy</a>
       </p>
 
-      <Rules /> 
+      <Rules />
     {/if}
     <button on:click={nextStep}>Continue</button>
   {:else if step === 1}
-  <fieldset>
-    {#if travels  == 0}
-
+    <fieldset>
+      {#if travels == 0}
         <legend>Enter your Data!</legend>
 
         <p>
-          We would kindly ask you to enter your name, e-mail-address and a password. This way we can record you as the original holder of this coin.
-          We promise you, that we will never ever publish your personal data _anywhere_! We might however use your e-mail-address to update you about what is going on with this coin. 
-          If you don't want this: we're working hard on a detailed concept. Let us know what you think!
+          We would kindly ask you to enter your name, e-mail-address and a
+          password. This way we can record you as the original holder of this
+          coin. We promise you, that we will never ever publish your personal
+          data _anywhere_! We might however use your e-mail-address to update
+          you about what is going on with this coin. If you don't want this:
+          we're working hard on a detailed concept. Let us know what you think!
         </p>
         {#if error_position == true}
-        <span class="error">Unfortunately you didn't allow us to set your location. We will automatically set it to Berlin</span>
+          <span class="error"
+            >Unfortunately you didn't allow us to set your location. We will
+            automatically set it to Berlin</span>
         {/if}
-         <label>
-
+        <label>
           <span>Original holder's name</span>
-          <input bind:value={recipientName} type="text" placeholder="Your name" />
+          <input
+            bind:value={recipientName}
+            type="text"
+            placeholder="Your name" />
         </label>
 
         <label>
@@ -246,32 +288,39 @@ const checkName = () => {
         <label>
           <span>Original holder's password</span>
 
-          <input
-            bind:value={recipientPassword}
-            type="password"/>
+          <input bind:value={recipientPassword} type="password" />
         </label>
 
         <label>
-          <input 
-          bind:checked={dataProtection}
-          type="checkbox"/>
-          <span>I consent that the entered data including my location and the story I enter within the next step will be used for this art project. Name, location and story will be published. Email address will be used to inform you about ... You can withdraw your consent any time for the future. For details please refer to the <a href="/privacy">privacy policy</a> </span>
-        </label> 
-
-    {:else}
+          <input bind:checked={dataProtection} type="checkbox" />
+          <span>
+            I consent that the entered data including my location and the story
+            I enter within the next step will be used for this art project.
+            Name, location and story will be published. Email address will be
+            used to inform you about ... You can withdraw your consent any time
+            for the future. For details please refer to the <a href="/privacy"
+              >privacy policy</a>
+          </span>
+        </label>
+      {:else}
         <legend>Handover Entry</legend>
 
         <p>
           Now it's time to enter the name of the person
-          <strong>receiving the coin</strong>, as well as their password and other
-          data
+          <strong>receiving the coin</strong>, as well as their password and
+          other data
         </p>
         {#if error_position == true}
-        <span class="error">Unfortunately you didn't allow us to set your location. We will automatically set it to Berlin</span>
+          <span class="error"
+            >Unfortunately you didn't allow us to set your location. We will
+            automatically set it to Berlin</span>
         {/if}
         <label>
           <span>Receiving person's name</span>
-          <input bind:value={recipientName} type="text" placeholder="Your name" />
+          <input
+            bind:value={recipientName}
+            type="text"
+            placeholder="Your name" />
         </label>
 
         <label>
@@ -292,58 +341,86 @@ const checkName = () => {
             placeholder="Your name" />
         </label>
 
-
         <label>
-          <input 
-          bind:checked={dataProtection}
-          type="checkbox"/>
-           <span>I consent that the entered data including my location and the story I enter within the next step will be used for this art project. Name, location and story will be published. Email address will be used to inform you about ... You can withdraw your consent any time for the future. For details please refer to the <a href="/privacy">privacy policy</a></span>
+          <input bind:checked={dataProtection} type="checkbox" />
+          <span
+            >I consent that the entered data including my location and the story
+            I enter within the next step will be used for this art project.
+            Name, location and story will be published. Email address will be
+            used to inform you about ... You can withdraw your consent any time
+            for the future. For details please refer to the <a href="/privacy"
+              >privacy policy</a
+            ></span>
         </label>
-      
-    {/if}
-  {#if data_protection_checked == false}
-    <span class="error">You need to accept the data proection clause!</span>
-  {/if}
-  {#if email_correct == false}
-    <span class="error">Please enter a valid email address!</span>
-  {/if}
-  {#if name_correct == false}
-    <span class="error">Please enter a valid name!</span>
-  {/if}
-  </fieldset>
-  <Paging {prevStep} {nextStep} />
-
-
-  {:else if step == 2} 
+      {/if}
+      {#if data_protection_checked == false}
+        <span class="error">You need to accept the data proection clause!</span>
+      {/if}
+      {#if email_correct == false}
+        <span class="error">Please enter a valid email address!</span>
+      {/if}
+      {#if name_correct == false}
+        <span class="error">Please enter a valid name!</span>
+      {/if}
+    </fieldset>
+    <Paging {prevStep} {nextStep} />
+  {:else if step == 2}
     <fieldset>
-    {#if travels == 0}
+      <legend>Your Location</legend>
+
       <p>
-        Thank you! Now almost set to hand over this coin to another person! 
-        Please let the world know how you received this coin!
+        Now please give us your location. You can either choose whether you want
+        to give us your location, or whether you want to choose one on the map
       </p>
       <label>
-        <span>Your Story</span>
-
-        <textarea bind:value={handoverText} />
+        <input
+          bind:checked={isCheckedLocation}
+          on:change={setLocation}
+          type="checkbox" />
+        <span> I want the browser to choose my location </span>
       </label>
-    {:else} 
+      <p>
+        <span
+          ><button on:click={chooseLocation} type="button" class="full"
+            >I want to choose my own location</button
+          ></span>
+      </p>
+      {#if error_submitting == true}
+        <span class="error"
+          >Something went wrong - retry, reload or contact us if it persists</span>
+      {/if}
+    </fieldset>
+    <Paging {prevStep} {nextStep} />
+  {:else if step == 3}
+    <fieldset>
+      {#if travels == 0}
+        <p>
+          Thank you! Now almost set to hand over this coin to another person!
+          Please let the world know how you received this coin!
+        </p>
+        <label>
+          <span>Your Story</span>
+
+          <textarea bind:value={handoverText} />
+        </label>
+      {:else}
         <legend>Handover Story</legend>
 
         <p>
-          Now please tell us your story. It would be very nice of you if you could
-          allow the phone to retrieve your location data!
+          Now please tell us your story. It would be very nice of you if you
+          could allow the phone to retrieve your location data!
         </p>
 
         <label>
           <span>Your Story</span>
-         <textarea bind:value={handoverText} />
+          <textarea bind:value={handoverText} />
         </label>
         {#if error_submitting == true}
-        <span class="error">Something went wrong - retry, reload or contact us if it persists</span>
-        {/if}    
+          <span class="error"
+            >Something went wrong - retry, reload or contact us if it persists</span>
+        {/if}
       {/if}
-      </fieldset>
-      <Paging {prevStep} {nextStep} />
+    </fieldset>
+    <Paging {prevStep} {nextStep} />
   {/if}
 </form>
-
